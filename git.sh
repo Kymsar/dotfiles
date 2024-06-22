1f8b6794a9dd4c2ff41db1c6891a1f6270193665
Kymsar <63454800+Kymsar@users.noreply.github.com>
2024-06-23 01:47:08 +0200

    feat(git): add git.sh file to set global git configs

---
+++ git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set git configurations
+git config --global user.email "63454800+Kymsar@users.noreply.github.com"
+git config --global user.name "Kymsar"
+
+echo "Git configurations set successfully."
